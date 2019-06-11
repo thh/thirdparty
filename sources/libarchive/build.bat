17c6f54f7fcb62a8df37ec3bcd0e99f50bc4cb52
thh01217 <thh01217@gmail.com>
2019-06-12 02:49:14 +0800

    add libarchive

---
+++ sources/libarchive/build.bat
@@ -0,0 +1,3 @@
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=D:\works\external_library\cxxlibs -D_BZIP2_PATHS=D:\works\external_library\cxxlib -DCMAKE_BUILD_TYPE=Release ..
+
+@rem -DBZIP2_LIBRARIES=D:\works\external_library\cxxlibs\lib
